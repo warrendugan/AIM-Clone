290c93e0cca8a8c93521975a562872d34a8a0753
Warren Dugan <warrendugan@gmail.com>
2017-02-07 10:08:45 -0800

    Pushing latest changes.

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
-const { app, BrowserWindow } = require('electron')
-const menubar = require('menubar')
-const mb = menubar()
+const { app, Tray, Menu, BrowserWindow } = require('electron')
 const path = require('path')
 const url = require('url')
-
-let mainWindow
+require('electron-reload')(__dirname)
+const iconPath = path.join(__dirname, 'icons/icon.png')
+let appIcon = null
+let win = null
+let mainWindow = null
 
 const createWindow = () => {
   mainWindow = new BrowserWindow({
-    width: 800,
+    width: 300,
     height: 600,
-    resizable: false
+    resizable: false,
+    backgroundColor: '#ECE9D7'
   })
+
+  win = new BrowserWindow({show: false})
+  appIcon = new Tray(iconPath)
+  var contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Toggle DevTools',
+      accelerator: 'Alt+Command+I',
+      click: function() {
+        mainWindow.show();
+        mainWindow.toggleDevTools();
+      }
+    },
+    {
+      label: 'Quit',
+      accelerator: 'Command+Q',
+      selector: 'terminate:',
+    }
+  ])
+  appIcon.setToolTip('AIM For Success.')
+  appIcon.setContextMenu(contextMenu)
 
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
-
-  // mainWindow.webContents.openDevTools()
 
   mainWindow.on('closed', () => {
     mainWindow = null
