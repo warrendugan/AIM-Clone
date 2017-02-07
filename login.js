f00312c0ddec3eab366de2e60147b076d55f4d91
Warren Dugan <warrendugan@gmail.com>
2017-02-07 10:08:45 -0800

    Pushing latest changes.

---
+++ login.js
@@ -0,0 +1,12 @@
+<img id="logo" src="./icons/login.png"/>
+<form>
+  <div id="login">
+    <br><label>ScreenName</label><br>
+    <input type="text" autofocus><br>
+    <br><label>Password</label><br>
+    <input type="password"><br>
+  </div>
+  <input id="submit" type="submit">
+</form>
+<div id="version">Version 1.999</div>
+<a id="forgot" href="#">Forgot Password?</a>
